d7da54b33f7eb653529b2e94626263cf047df828
Shan Ali <shaneali720@gmail.com>
2025-11-14 07:06:02 +0000

    Try fixing this error: `Runtime Error: Error: 2 UNKNOWN: Getting metadat

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,15 @@ const nextConfig: NextConfig = {
         "https-6000-firebase-studio-1762452668457.cluster-fo5feun3fzf2etidpi3ckpp6te.cloudworkstations.dev",
         "https-6000-firebase-sellaya-backup-01-1762673440166.cluster-fo5feun3fzf2etidpi3ckpp6te.cloudworkstations.dev"
     ],
+    serverActions: {
+      bodySizeLimit: '2mb',
+      serverActions: {
+        bodySizeLimit: '2mb',
+      },
+    },
+  },
+  serverActions: {
+    bodySizeLimit: '2mb',
   },
 };
 
